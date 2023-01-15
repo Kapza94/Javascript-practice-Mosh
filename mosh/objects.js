1cfbcd9af95ef20bd1cbd67ad65dedda35dc0c8b
Kapza94 <108750035+Kapza94@users.noreply.github.com>
2023-01-15 21:13:05 +0100

    Operators\

---
+++ mosh/objects.js
@@ -0,0 +1,17 @@
+
+let person = {
+    name: 'Luka',
+    age: 30
+    }
+    
+    //how to access properties in an object:
+    //Dot notation
+    person.name = 'John';//should be default choice because shorter.
+
+    //can also accesss property with 
+    //Bracket notation
+    person['name'] = 'Mary';
+
+
+console.log(person.name);// this is how you would access this property when logging. 
+
